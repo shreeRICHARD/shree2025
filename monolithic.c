ab026049c4ca27325ff13fba1a4016677d16fbb7
shreeRICHARD <shreerichard20@gmail.com>
2025-10-27 20:36:50 +0530

    Add project files

---
+++ monolithic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("Sum: %d\n", sum);
+
+    // More unrelated logic could follow here within main
+    // e.g., calculating product, printing messages, etc.
+
+    return 0;
+}
